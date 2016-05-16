e6cfb05b2cbe38c259f54af6bad4a82e29d160c5
StanleyTack <stanley.tack@users.noreply.github.com>
2016-05-16 18:09:23 -0500

    created characters and actor tables

---
+++ app/models/character.rb
@@ -0,0 +1,10 @@
+class Character < ActiveRecord::Base
+
+ #  Character:
+ # - movie_id: must be present
+ # - actor_id: must be present
+ validates :movie_id, :actor_id, presence: true
+
+ # - name: no rules
+
+end
