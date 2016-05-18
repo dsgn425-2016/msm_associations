7703b6c89854eff2289f8f64fbca32dd0bee7f64
maggiehsu <maggiehsu@users.noreply.github.com>
2016-05-17 19:55:44 -0500

    maggie

---
+++ app/models/movie.rb
@@ -0,0 +1,17 @@
+class Movie < ActiveRecord::Base
+
+ # - director_id: must be present
+ validates :director_id, :presence=>true
+ # - title: must be present; must be unique in combination with year
+ validates :title, :presence=>true, uniqueness: { scope: :year,
+    message: "already exists in this year" }
+
+ # - year: must be integer between 1870 and 2050
+ validates :year, :presence=>true, numericality: { only_integer: true, less_than_or_equal_to:2050, greater_than_or_equal_to:1870 }
+ # - duration: must be integer between 0 and 2764800
+ validates :duration, :presence=>true, numericality: {only_integer: true, less_than_or_equal_to:2764800, greater_than_or_equal_to:0 }
+ # - description: no rules
+ # - image_url: no rules
+
+
+end
