f085b7e1fb74e41f965ded1f09eaf414cd1628f4
Tyler Hardy <thardy2017@kellogg.northwestern.edu>
2016-05-18 11:22:25 -0500

    added actors and characters

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,7 +1,6 @@
 class Director < ActiveRecord::Base
   #  - name: must be present; must be unique in combination with dob
 validates :name, :presence => true, :uniqueness => {:scope => :dob }
-
   #  - dob: no rules
   #  - bio: no rules
   #  - image_url: no rules
