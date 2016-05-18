2cbd74da58385ccbf747fbb0968df060295e60e5
jpetersen12 <jpetersen12@users.noreply.github.com>
2016-05-18 14:27:59 -0500

    Find_bys and Wheres cleaned from Actors and Characters

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -3,7 +3,7 @@ class Actor < ActiveRecord::Base
  #  Actor:
  # - name: must be present; must be unique in combination with dob
  validates :name, :presence =>true, :uniqueness => {:scope => :dob}
-
+ has_many(:characters, :class_name => "Character", :foreign_key => "actor_id")
  # - dob: no rules
  # - bio: no rules
  # - image_url: no rules
