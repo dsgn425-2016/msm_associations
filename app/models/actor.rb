2e90a57e7d381ddd29058fad7303698b5ce46496
jpetersen12 <jpetersen12@users.noreply.github.com>
2016-05-23 14:01:48 -0500

    Completed functionality

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -4,6 +4,7 @@ class Actor < ActiveRecord::Base
  # - name: must be present; must be unique in combination with dob
  validates :name, :presence =>true, :uniqueness => {:scope => :dob}
  has_many(:characters, :class_name => "Character", :foreign_key => "actor_id")
+ has_many(:movies, :through => :characters)
  # - dob: no rules
  # - bio: no rules
  # - image_url: no rules
