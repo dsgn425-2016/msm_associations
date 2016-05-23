03d8631e3bf53a2f1ae2d8882a8df892dea51196
axepolzin <apolzin2017@users.noreply.github.com>
2016-05-23 14:06:27 -0500

    hw

---
+++ app/models/director.rb
@@ -0,0 +1,11 @@
+class Director < ActiveRecord::Base
+
+
+validates :name, :presence => true, :uniqueness => {:scope => :dob }
+
+
+# - name: must be present; must be unique in combination with dob
+ #- dob: no rules
+ #- bio: no rules
+ #- image_url: no rules
+end
