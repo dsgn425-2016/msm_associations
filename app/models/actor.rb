9ad6096761285854b95f1c09ec6f3e36dc039fc9
jgoldstein2017 <jgoldstein2017@kellogg.northwestern.edu>
2016-05-23 01:20:02 -0500

    new-model-rules

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,6 +2,9 @@ class Actor < ActiveRecord::Base
 # - name: must be present; must be unique in combination with dob
   validates :name, :presence => true, :uniqueness => { :scope => :dob }
 
+  has_many(:characters, :class_name => "Character", :foreign_key => "actor_id")
+
+
 # - dob: no rules
 # - bio: no rules
 # - image_url: no rules
