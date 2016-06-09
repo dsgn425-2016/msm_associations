d135755d29946a5a4049a0acd405b4a5cf1288e1
boizie <johngwonchoi@MacBook-Pro-3.local>
2016-06-09 10:23:16 -0500

    msm

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,7 +1,7 @@
 class Director < ActiveRecord::Base
-# - name: must be present; must be unique in combination with dob
+  # - name: must be present; must be unique in combination with dob
+  # - dob: no rules
+  # - bio: no rules
+  # - image_url: no rules
 
-# - dob: no rules
-# - bio: no rules
-# - image_url: no rules
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,38 @@
 Rails.application.routes.draw do
+  # Routes for the Character resource:
+  # CREATE
+  get "/characters/new", :controller => "characters", :action => "new"
+  post "/create_character", :controller => "characters", :action => "create"
+
+  # READ
+  get "/characters", :controller => "characters", :action => "index"
+  get "/characters/:id", :controller => "characters", :action => "show"
+
+  # UPDATE
+  get "/characters/:id/edit", :controller => "characters", :action => "edit"
+  post "/update_character/:id", :controller => "characters", :action => "update"
+
+  # DELETE
+  get "/delete_character/:id", :controller => "characters", :action => "destroy"
+  #------------------------------
+
+  # Routes for the Actor resource:
+  # CREATE
+  get "/actors/new", :controller => "actors", :action => "new"
+  post "/create_actor", :controller => "actors", :action => "create"
+
+  # READ
+  get "/actors", :controller => "actors", :action => "index"
+  get "/actors/:id", :controller => "actors", :action => "show"
+
+  # UPDATE
+  get "/actors/:id/edit", :controller => "actors", :action => "edit"
+  post "/update_actor/:id", :controller => "actors", :action => "update"
+
+  # DELETE
+  get "/delete_actor/:id", :controller => "actors", :action => "destroy"
+  #------------------------------
+
   # Routes for the Movie resource:
   # CREATE
   get "/movies/new", :controller => "movies", :action => "new"
@@ -33,58 +67,4 @@ Rails.application.routes.draw do
   get "/delete_director/:id", :controller => "directors", :action => "destroy"
   #------------------------------
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
